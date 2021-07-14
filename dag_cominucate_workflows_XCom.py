8cc1a834d47cc8bf9d356e2fa915cb4ee84801d3
gustavo9601 <tavo9601@gmail.com>
2021-07-13 23:11:11 -0500

    ways to create flow of workflow

--- dag_cominucate_workflows_XCom.py
+++ dag_cominucate_workflows_XCom.py
@@ -6,7 +6,6 @@ from airflow.models import DAG
 """
 Imports operators
 """
-from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 
@@ -29,12 +28,13 @@ def hello_world_loop():
     # Debe retornar algo, para que sea recibido por el Xcom
     return grettings
 
+
 def prueba_pull(**context):
     # accedemos a la propiedad
     task_instance = context['task_instance']
     # para obtener el retorno de un proceso del workflow especificamos su task_id
     value_pull = task_instance.xcom_pull(task_ids='prueba_python')
-    
+
     print(f'Data obtenida desde el callback, {value_pull}')
 
 
@@ -42,7 +42,6 @@ def prueba_pull(**context):
 with DAG('dag_test_comunicate_workflows',
          default_args=default_args,
          schedule_interval='@daily') as dag:
-
     # Inciando otro operator
     # pasando una funcion callback a ejecutar
     # do_xcom_push // notifica a workflow retornara un valor desde el callback
@@ -52,10 +51,9 @@ with DAG('dag_test_comunicate_workflows',
 
     #   provide_context=True // especifica que el callback recibira parametros emitidos por otros workflows
     prueba_pull_workflow = PythonOperator(task_id='prueba_pull_workflow',
-                                   python_callable=prueba_pull,
-                                   provide_context=True,
-                                   do_xcom_push=True)
-
+                                          python_callable=prueba_pull,
+                                          provide_context=True,
+                                          do_xcom_push=True)
 
     # Iniciando otro operator que ejecuta via bash un script
     # bash_command // recibe la ruta y el comando a ejecutar
@@ -64,4 +62,14 @@ with DAG('dag_test_comunicate_workflows',
                                bash_command='python C/Users/gustavomp/Pictures/bd/docker_files/test.py')
 
 # Define el orden de ejecucion de cada operator
-prueba_python >> prueba_pull_workflow  >> prueba_bash
+prueba_python >> prueba_pull_workflow >> prueba_bash
+
+# Otra forma
+# [prueba_python, prueba_pull_workflow, prueba_bash]
+
+# Creando otra rama de proceso al tiempo
+# prueba_python >> prueba_pull_workflow
+# prueba_python >> prueba_bash
+# // se toma los ultimos task_id que determinan el fin del proceso
+#[prueba_pull_workflow, prueba_bash] >> fin_proceso_task_id
+
